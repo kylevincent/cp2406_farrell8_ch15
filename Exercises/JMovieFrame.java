e8d4fc8229160d6efe35435e99917cb6d5eaf0f3
jc431075 <kyle.vincent@my.jcu.edu.au>
2017-10-06 09:54:01 +1000

    on ex1

---
+++ Exercises/JMovieFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by jc431075 on 6/10/17.
+ */
+public class JMovieFrame extends JFrame implements ActionListener
+{
+    JButton northButton = new JButton ("Noisy north");
+    JButton southButton = new JButton ("South side");
+    JButton eastButton = new JButton ("East bois");
+    JButton westButton = new JButton ("West woes");
+    JLabel northLabel = new JLabel ("NOOT NOOT");
+    JLabel southLabel = new JLabel ("bicycle riding");
+    JLabel eastLabel = new JLabel ("right is tight");
+    JLabel westLabel = new JLabel ("left is the best");
+
+    p
+}
